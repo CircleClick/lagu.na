c3b083636e5f515673fadd567f83e2e2721c75ea
calebbabin <object.caleb@gmail.com>
2022-04-12 14:13:51 -0500

    properly preload as font

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,10 @@ export default class MyDocument extends Document {
 				<Head>
 					<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 
-					<link rel="preload" href="/fonts/OBMPlexSans-Bold.woff2" />
-					<link rel="preload" href="/fonts/OBMPlexSans-Regular.woff2" />
-					<link rel="preload" href="/fonts/Inter-Bold.woff2" />
-					<link rel="preload" href="/fonts/Inter-Regular.woff2" />
+					<link rel="preload" href="/fonts/OBMPlexSans-Bold.woff2" as="font" />
+					<link rel="preload" href="/fonts/OBMPlexSans-Regular.woff2" as="font" />
+					<link rel="preload" href="/fonts/Inter-Bold.woff2" as="font" />
+					<link rel="preload" href="/fonts/Inter-Regular.woff2" as="font" />
 				</Head>
 				<body>
 					<Main />
